03d84247ebda74560450db540638b6de3dced54d
hoaik53 <hoaik53@gmail.com>
2020-02-14 22:12:34 +0700

    Revert "BAN CHUAN"
    This reverts commit 1cde4c8078b68cfd4f7b22c97cc0df4792b6fad9.

--- hcms/modules/about.php
+++
@@ -1,4 +0,0 @@
-<?php if (substr_count($_SERVER['PHP_SELF'],'/about.php')>0) die ("You can't access this file directly..."); ?>
-<p class="topmenu" style="padding: 96 36 96 36">
-<?php echo $strBCMS; ?>
-</p>
